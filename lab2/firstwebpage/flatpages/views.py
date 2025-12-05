6b10fdea034b78806f7c4e75837f09f200ceacf5
AnimeySun <veniamin.startzev@gmail.com>
2025-12-06 02:09:57 +0300

    Add files via upload

---
+++ lab2/firstwebpage/flatpages/views.py
@@ -0,0 +1,8 @@
+﻿from django.shortcuts import render
+from django.http import HttpResponse
+from django import template
+# Create your views here.
+def home(request):
+    return render(request, 'templates/index.html')
+def static_handler(request):
+    return render(request, 'static_handler.html')
